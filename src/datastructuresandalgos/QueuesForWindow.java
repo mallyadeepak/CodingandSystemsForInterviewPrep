2cb8980518d3f8ee18f9b0dd66b08b5bdee59e2b
Deepak <mallyadeepak@gmail.com>
2025-06-01 20:12:18 +0000

    Adding a comment for QueuesForWindow

--- src/datastructuresandalgos/QueuesForWindow.java
+++ src/datastructuresandalgos/QueuesForWindow.java
@@ -3,6 +3,15 @@ package datastructuresandalgos;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+
+
+/**
+ * This class QueuesForWindow demonstrates how to use 
+ * Queues in a window fashion specfically for solving 
+ * sliding-window problems. The idea is you expand the window
+ * on the right by adding elements on right, and shrink the window from the left by removing elements
+ * from the left.
+ */
 public class QueuesForWindow {
 
     public static void main(String[] args) {
